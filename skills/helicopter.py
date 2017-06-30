821e54b34d502941e5a3922f6d40d026d54c5de0
stepanovep <stepanovep@gmail.com>
2017-07-01 01:42:11 +0300

    skill maximizer - fly around by helicopter

---
+++ skills/helicopter.py
@@ -0,0 +1,24 @@
+from directKeys import *
+from playUtils import pause_before_start
+
+
+def fly_around(seconds):
+    press_keys([W, NP_8])
+    time.sleep(4)
+    press_key(A)
+
+    for i in range(1, seconds):
+        if i % 15 == 0:
+            hold_key(NP_5, duration=1)
+        time.sleep(1)
+
+    release_keys([W, NP_8, A])
+
+
+def main():
+    pause_before_start(8)
+    fly_around(60)
+
+
+if __name__ == '__main__':
+    main()
