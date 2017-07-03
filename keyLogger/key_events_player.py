aa0480d26ee216de2b9ceb9d1803c877a6c25e64
stepanovep <stepanovep@gmail.com>
2017-07-03 20:04:23 +0300

    key events Recorder and Executor

---
+++ keyLogger/key_events_player.py
@@ -0,0 +1,43 @@
+import sched
+
+from utils.common_commands import *
+
+
+def read_record():
+    with open('loop.txt', 'r') as f:
+        for line in f:
+            event = line.strip().split()
+            event_starts_time = float(event[0][:5])
+            key_raw = event[1]
+            action = event[2]
+            key = get_key_from_str(key_raw)
+
+            if key_raw == 'Key.backspace':
+                break
+
+            events.append((event_starts_time, key, action))
+
+
+events = list()
+read_record()
+pause_before_start(5)
+
+
+def main():
+    while True:
+        press_and_release_key(BACKSPACE)
+        time.sleep(2)
+        s = sched.scheduler(time.time, time.sleep)
+        for event in events:
+            if event[2] == 'pressed':
+                s.enter(event[0], 1, press_key, argument=(event[1],))
+            else:
+                s.enter(event[0], 1, release_key, argument=(event[1],))
+
+        s.run()
+        time.sleep(70)
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ keyLogger/key_events_recorder.py
@@ -0,0 +1,38 @@
+import time
+from pynput import keyboard
+from utils.common_commands import pause_before_start
+
+
+def on_press(key):
+    if key not in pressed_keys:
+        pressed_keys.add(key)
+        events.append(((time.time() - start_time), key, 'pressed'))
+
+
+def on_release(key):
+    pressed_keys.discard(key)
+    events.append(((time.time() - start_time), key, 'released'))
+    if key == keyboard.Key.backspace:
+        return False
+
+
+pressed_keys = set()
+events = list()
+
+pause_before_start(5)
+start_time = time.time()
+
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
+
+print('keys are pressing : {}'.format(pressed_keys))
+print(*events, sep='\n', end='\n')
+
+
+with open('race_record.txt', 'w') as f:
+    for event in events:
+        f.write(str(event[0]) + ' ')
+        f.write(str(event[1]).strip('\'') + ' ')
+        f.write(event[2] + '\n')
